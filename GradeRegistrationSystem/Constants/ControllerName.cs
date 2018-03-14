cf2f389f26e393070f63a4032c43ddbe7be4400c
Mike <Mike@DESKTOP-5IHVOMU>
2018-03-14 23:51:25 +0100

    Project initialization

---
+++ GradeRegistrationSystem/Constants/ControllerName.cs
@@ -0,0 +1,8 @@
+﻿namespace GradeRegistrationSystem.Constants
+{
+    public static class ControllerName
+    {
+        public const string Error = "Error";
+        public const string Home = "Home";
+    }
+}
---
+++ GradeRegistrationSystem/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GradeRegistrationSystem.MvcApplication" Language="C#" %>
