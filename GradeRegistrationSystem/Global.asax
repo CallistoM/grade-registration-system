be84bff8e4b9034d1934a5eafc729ecbeddda641
Mike <Mike@DESKTOP-5IHVOMU>
2018-05-19 14:30:28 +0200

    Removed actual project and initialized new project

--- GradeRegistrationSystem/Global.asax
+++
@@ -1,1 +0,0 @@
-﻿<%@ Application Codebehind="Global.asax.cs" Inherits="GradeRegistrationSystem.MvcApplication" Language="C#" %>
---
+++ WebApplication3/Controllers/scoresController.cs
@@ -0,0 +1,140 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using Microsoft.AspNet.Identity;
+using Microsoft.AspNet.Identity.Owin;
+using Microsoft.Owin.Security;
+using WebApplication3.Models;
+
+namespace WebApplication3.Controllers
+{
+    [Authorize]
+    public class scoresController : Controller
+    {
+        private Entities1 db = new Entities1();
+
+        // GET: scores
+        public ActionResult Index()
+        {
+            var scores = db.scores.Include(s => s.course).Include(s => s.student);
+            return View(scores.ToList());
+        }
+
+        // GET: scores/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            score score = db.scores.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            return View(score);
+        }
+
+        // GET: scores/Create
+        public ActionResult Create()
+        {
+            ViewBag.course_id = new SelectList(db.courses, "id", "name");
+            ViewBag.student_id = new SelectList(db.students, "id", "name");
+            return View();
+        }
+
+        // POST: scores/Create
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "id,course_id,student_id,score1")] score score)
+        {
+            if (ModelState.IsValid)
+            {
+                db.scores.Add(score);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            ViewBag.course_id = new SelectList(db.courses, "id", "name", score.course_id);
+            ViewBag.student_id = new SelectList(db.students, "id", "name", score.student_id);
+            return View(score);
+        }
+
+        // GET: scores/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            score score = db.scores.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.course_id = new SelectList(db.courses, "id", "name", score.course_id);
+            ViewBag.student_id = new SelectList(db.students, "id", "name", score.student_id);
+            return View(score);
+        }
+
+        // POST: scores/Edit/5
+        // To protect from overposting attacks, please enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "id,course_id,student_id,score1")] score score)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(score).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.course_id = new SelectList(db.courses, "id", "name", score.course_id);
+            ViewBag.student_id = new SelectList(db.students, "id", "name", score.student_id);
+            return View(score);
+        }
+
+        // GET: scores/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            score score = db.scores.Find(id);
+            if (score == null)
+            {
+                return HttpNotFound();
+            }
+            return View(score);
+        }
+
+        // POST: scores/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            score score = db.scores.Find(id);
+            db.scores.Remove(score);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
